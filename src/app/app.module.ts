4bdd1f4bcdd5e99c9d6c928f9aa5408f4e2f1d99
sguha-work <sguha1988.life@gmail.com>
2018-09-29 12:13:04 +0530

    routing module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import custom created routing module
+import {AppRoutingModule} from './app-routing/app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -19,12 +22,12 @@ import { ProductComponent } from './product/product.component';
     ProductComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [
     AppComponent,
-    HomeComponent,
   ]
 })
 export class AppModule { }
