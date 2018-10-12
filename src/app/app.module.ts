3ada00c940eb46c3e8883d2708ee66257a894baa
gopaalbiswas <gopal.biswas24@gmail.com>
2018-10-12 20:02:24 +0530

    Two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-// import custom created routing module
-import {AppRoutingModule} from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -10,6 +9,11 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+
 
 
 @NgModule({
@@ -20,14 +24,18 @@ import { ProductComponent } from './product/product.component';
     FooterComponent,
     AboutComponent,
     ProductComponent,
+    ServiceComponent,
+    ContactComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [
     AppComponent,
+    HomeComponent,
   ]
 })
 export class AppModule { }
